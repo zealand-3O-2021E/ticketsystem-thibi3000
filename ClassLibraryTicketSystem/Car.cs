21b2b2f6c04edcbbe94debe7638439a4de9812b3
thibo <thibo@LAPTOP-1FMMDL4N>
2021-09-28 16:00:03 +0200

    Solving of Task2

---
+++ ClassLibraryTicketSystem/Car.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ClassLibraryTicketSystem
+{
+    /// <summary>
+    /// Class for a car
+    /// </summary>
+    public class Car
+    {
+        public string Licenseplate { get; set; }
+
+        public DateTime Date { get; set; }
+
+        public Car(string plate, DateTime date)
+        {
+            Licenseplate = plate;
+            Date = date;
+        }
+
+
+        /// <summary>
+        /// Method that returns a price for the cars crossing the bridge
+        /// </summary>
+        /// <returns>240</returns>
+        public double Price()
+        {
+            return 240;
+        }
+        /// <summary>
+        /// Method that returns the type of vehicle
+        /// </summary>
+        /// <returns>Car</returns>
+        public string VehicleType()
+        {
+            return "Car";
+        }
+    }
+}
---
+++ ClassLibraryTicketSystemTests/CarTests.cs
@@ -0,0 +1,42 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using ClassLibraryTicketSystem;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ClassLibraryTicketSystem.Tests
+{
+    [TestClass()]
+    public class CarTests
+    {
+        [TestMethod()]
+        public void priceTest()
+        {
+            //Arrange
+            DateTime datetime = new DateTime(2021, 09, 25);
+            Car car = new("1-ABC-12", datetime);
+
+            //Act
+            double price = car.Price();
+
+            //Assert
+            Assert.AreEqual(240, price);
+        }
+
+            [TestMethod()]
+        public void VehicleTypeTest()
+        {
+            //Arrange
+            DateTime datetime = new DateTime(2021, 09, 25);
+            Car car = new("1-ABC-12", datetime);
+
+            //Act
+            string type = car.VehicleType();
+
+            //Assert
+            Assert.AreEqual("Car", type);
+        }
+    }
+}
