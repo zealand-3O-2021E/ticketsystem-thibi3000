964b187f07ae4b1d86f886527b8ad985a01ed6ec
thibo <thibo@LAPTOP-1FMMDL4N>
2021-09-29 13:32:27 +0200

    Solving of Task9

---
+++ StoreBaeltTicketLibrary/StorebaeltsbroenTicketSystem.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using ClassLibraryTicketSystem;
+
+namespace StoreBaeltTicketLibrary
+{
+    public class StorebaeltsbroenTicketSystem
+    {        
+        public ClassLibraryTicketSystem.Car car;
+        public double price;
+
+        public StorebaeltsbroenTicketSystem()
+        {
+            car = new ClassLibraryTicketSystem.Car("1-AB-1", new DateTime(2021, 09, 25));
+            price = 0;
+        }
+
+        public double CarPrice(bool weekend, bool brobizz)
+        {
+            if(weekend)
+            {
+                if (brobizz)
+                {
+                    return (car.Price(false) * 0.80)*0.95;
+                }
+                return car.Price(false) * 0.80;
+            }
+            if (brobizz)
+            {
+                return car.Price(true);
+            }
+            return car.Price(false);
+        }
+    }
+}
---
+++ StoreBaeltTicketLibraryTests/StorebaeltsbroenTicketSystemTests.cs
@@ -0,0 +1,67 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using StoreBaeltTicketLibrary;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace StoreBaeltTicketLibrary.Tests
+{
+    [TestClass()]
+    public class StorebaeltsbroenTicketSystemTests
+    {      
+
+        [TestMethod()]
+        public void CarPriceNoBrobizzNoWeekendTest()
+        {
+            //Arrange
+            StorebaeltsbroenTicketSystem store = new StorebaeltsbroenTicketSystem();
+
+            //Act
+            double price = store.CarPrice(false, false);
+
+            //Assert
+            Assert.AreEqual(240, price);
+        }
+
+        [TestMethod()]
+        public void CarPriceBrobizzNoWeekendTest()
+        {
+            //Arrange
+            StorebaeltsbroenTicketSystem store = new StorebaeltsbroenTicketSystem();
+
+            //Act
+            double price = store.CarPrice(false, true);
+
+            //Assert
+            Assert.AreEqual(228, price);
+        }
+
+        [TestMethod()]
+        public void CarPriceNoBrobizzWeekendTest()
+        {
+            //Arrange
+            StorebaeltsbroenTicketSystem store = new StorebaeltsbroenTicketSystem();
+
+            //Act
+            double price = store.CarPrice(true, false);
+
+            //Assert
+            Assert.AreEqual(192, price);
+        }
+
+        [TestMethod()]
+        public void CarPriceBrobizzWeekendTest()
+        {
+            //Arrange
+            StorebaeltsbroenTicketSystem store = new StorebaeltsbroenTicketSystem();
+
+            //Act
+            double price = store.CarPrice(true, true);
+
+            //Assert
+            Assert.AreEqual(182.4, price, 0.01);
+        }
+    }
+}
