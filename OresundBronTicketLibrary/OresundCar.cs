b9be703727d7e77b36e0c272c2bfa0d448566333
thibo <thibo@LAPTOP-1FMMDL4N>
2021-10-04 14:35:13 +0200

    Added some extra comments

--- OresundBronTicketLibrary/OresundCar.cs
+++ OresundBronTicketLibrary/OresundCar.cs
@@ -7,12 +7,20 @@ using ClassLibraryTicketSystem;
 
 namespace OresundBronTicketLibrary
 {
+    /// <summary>
+    /// This is the class for a car crossing the oresund bridge
+    /// </summary>
     public class OresundCar : Car
     {
         public OresundCar(string plate, DateTime date) : base(plate, date)
         {
         }
 
+        /// <summary>
+        /// This method returns the prize of the ticket for a car
+        /// </summary>
+        /// <param name="brobizz">this parameter is used to notice if a brobizz is used or not</param>
+        /// <returns></returns>
         public override double Price(bool brobizz)
         {
             if (brobizz)
@@ -22,6 +30,10 @@ namespace OresundBronTicketLibrary
             return 410;
         }
 
+        /// <summary>
+        /// This method returns the type of the vehicle crossing the bridge
+        /// </summary>
+        /// <returns></returns>
         public override string VehicleType()
         {
             return "Oresund car";
--- OresundBronTicketLibrary/OresundMC.cs
+++ OresundBronTicketLibrary/OresundMC.cs
@@ -9,12 +9,19 @@ namespace OresundBronTicketLibrary
 {
     public class OresundMC : MC
     {
-
+        /// <summary>
+        /// This is the class for a motorcycle crossing the oresund bridge
+        /// </summary>
         public OresundMC(string plate, DateTime date) : base(plate, date)
         {
 
         }
 
+        /// <summary>
+        /// This method returns the prize of the ticket for a motorcycle
+        /// </summary>
+        /// <param name="brobizz">this parameter is used to notice if a brobizz is used or not</param>
+        /// <returns></returns>
         public override double Price(bool brobizz)
         {
             if (brobizz)
@@ -24,6 +31,10 @@ namespace OresundBronTicketLibrary
             return 210;
         }
 
+        /// <summary>
+        /// This method returns the type of the vehicle crossing the bridge
+        /// </summary>
+        /// <returns></returns>
         public override string VehicleType()
         {
             return "Oresund MC";
